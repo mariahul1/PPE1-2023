122c8824736bcef032523f28d1ee1b09dc1426b7
mariahul1 <hul.maria88@gmail.com>
2023-10-30 11:48:34 +0100

    exercises_seance6.sh moved to exercises
    exercises_seance6.sh moved to exercises

--- exercises/exercises_seance6.sh
+++ exercises/exercises_seance6.sh
@@ -4,12 +4,12 @@
 while read -r line;
 do
 	echo ${line};
-done < "urls/fr.txt"
+done < "fr.txt"
 ## Questions 
 ## 1. Dans ce cas, c'est mieux d'utiliser la boucle while au lieu de la commande cat parce que la commande cat lit l'intégralité du fichier en mémoire en une seule fois, cela pourrait poser des problèmes si le fichier est trop grand pour tenir en mémoire pendant que la boucle while lit le fichier ligne par ligne.
 ## 2. En utilisant 'if' on vérifie si le fichie a une valeur et s'il existe, si les deux conditions sont remplies, on effectue la boucle while, sinon on s'arrête.
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 <urls/fr.txt>";
+    echo "Usage: $0 <fr.txt>";
     exit 1
 fi
 
@@ -26,6 +26,6 @@ while read -r line;
 do
     echo "line $ligne_numero : $line";
     ligne_numero=$((ligne_numero + 1));
-done < "urls/fr.txt"
+done < "fr.txt"
 
 ## 3. On ajoute 'ligne_numero=1', une variable pour compter les lignes. Après qu'on affiche une ligne avec la commande 'echo', on introduit la variable ligne_numero et on ajoute '1' pour passer au numéro de ligne suivant.
