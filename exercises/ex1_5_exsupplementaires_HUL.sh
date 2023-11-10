5faedcd682d514cda171de73ab24f34f3e607659
mariahul1 <hul.maria88@gmail.com>
2023-11-10 19:28:38 +0100

    Correction l'exercice 1
    Correction de l'exercice 1 des exercises supplementaires

--- exercises/ex1_5_exsupplementaires_HUL.sh
+++ exercises/ex1_5_exsupplementaires_HUL.sh
@@ -4,16 +4,17 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-candide.txt="$1"
+input_file="$1"
 
 if [ ! -f candide.txt ]; then
-    echo "Le fichier candide.txt n'existe pas."
+    echo "Le fichier input_file n'existe pas."
     exit 1
 fi
 
-tr -d '[:punct:]' < candide.txt > candide_2.txt
+tr -d '[:punct:]' < $input_file > candide_2.txt
 tr '[:upper:]' '[:lower:]' < candide_2.txt > candide_3.txt
 
 echo "Le traitement a été effectué. Le résultat est enregistré dans candide_3.txt."
+
 
 rm candide_2.txt
